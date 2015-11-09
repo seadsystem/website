f85b5998f3ab1ac67ca4851b7b87274ca8634e0b
Ian Lofgren <i.lof33@gmail.com>
2015-11-08 22:51:09 -0800

    basic api for total power of a device working, need to add error checking

--- api/views.py
+++ api/views.py
@@ -39,15 +39,52 @@ must be utc unix timestamps
 class TotalDevicePower(APIView):
     permission_classes = []
     allowed_parameters = ['start_time', 'end_time']
+    devices = ['Panel1', 'Panel2', 'Panel3']
 
     def get(self, request, device_id):
         params = request.GET.dict()
-
         for param in params:
             if param not in self.allowed_parameters:
                 raise ValueError('Query parameter not allowed: ', param)
 
-        params['type'] = 'P'
-        response = requests.get("http://db.sead.systems:8080/" + device_id, params=params)
+        start_time = request.GET.get('start_time')
+        end_time = request.GET.get('end_time')
+        url = "http://db.sead.systems:8080/" + device_id
 
-        return Response({"status":"helllo", "data": "cool dudee"})
+        params = dict()
+        params['type'] = 'P'
+        responseStart = []
+        responseEnd = []
+
+        for device in self.devices:
+            params['device'] = device
+            params['start_time'] = start_time
+            params['end_time'] = start_time
+            responseStart.append(requests.get(url, params).json())
+            params['start_time'] = end_time
+            params['end_time'] = end_time
+            responseEnd.append(requests.get(url, params).json())
+
+        def filter_out_irrelavant_values(time_item):
+            time_item.pop(0)
+            return time_item[0][1]
+
+        start_arr = list(map(filter_out_irrelavant_values, responseStart))
+        end_arr = list(map(filter_out_irrelavant_values, responseEnd))
+
+        total_power = 0
+
+        for i, power in enumerate(start_arr):
+            total_power += -int(end_arr[i]) + int(power)
+
+        print(total_power)
+
+
+        # for power in total_power_arr:
+         #   total_power += power
+
+        print("power1: ", start_arr)
+        print("power2: ", end_arr)
+        print("total_power: ", total_power)
+
+        return Response({"status": 200, "data": total_power})
