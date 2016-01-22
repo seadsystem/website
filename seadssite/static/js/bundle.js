23a8c4b2cc7126b6217fc0d789ad67974b91e975
ram <ramhound7@yahoo.com>
2016-01-21 16:55:08 -0800

    Added pie chart for testing

--- seadssite/static/js/bundle.js
+++ seadssite/static/js/bundle.js
@@ -16733,8 +16733,25 @@
 },{}],3:[function(require,module,exports){
 var c3 = require('c3');
 
-var chart = c3.generate({
+
+var pie = c3.generate({
     bindto: '#chart',
+    data: {
+        // iris data from R
+        columns: [
+            ['data1', 30],
+            ['data2', 120],
+        ],
+        type : 'pie',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    }
+});
+
+
+var chart1 = c3.generate({
+    bindto: '#chart2',
     data: {
       columns: [
         ['data1', 30, 200, 100, 400, 150, 250],
@@ -16742,4 +16759,5 @@ var chart = c3.generate({
       ]
     }
 });
+
 },{"c3":1}]},{},[3]);
