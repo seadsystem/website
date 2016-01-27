204e9fad352c3733250987c14edcbbe1423da275
ram <ramhound7@yahoo.com>
2016-01-26 16:50:21 -0800

    date picker added to show 24 hour energy usage for any day

--- seadssite/static/js/bundle.js
+++ seadssite/static/js/bundle.js
@@ -16731,47 +16731,55 @@
   this.d3 = d3;
 }();
 },{}],3:[function(require,module,exports){
+"use strict";
+
 var c3 = require('c3');
 
-var url = "http://db.sead.systems:8080/466419817?start_time=1446537600&end_time=1446796800&list_format=energy&type=P&device=Panel1&granularity=3600";
+//var url = "http://db.sead.systems:8080/466419818?start_time=1446537600&end_time=1446624000&list_format=energy&type=P&device=Panel1&granularity=3600";
 
-var test_request = new XMLHttpRequest();
-test_request.onreadystatechange = function() {
-    //console.log(test_request.readyState);
-    if (test_request.readyState == XMLHttpRequest.DONE) {
-        if (test_request.status == 200) {
-            console.log(test_request.responseText);
-            generate_chart1(JSON.parse(test_request.responseText));
-        } else {
-            console.log("it broke");
+function create_url(date) {
+    if (!date) {
+        date = Date.now();
+        var end = Math.floor(date / 1000);
+        var start = end - 86400;
+    } else {
+        var start = Math.floor(date / 1000);
+        var end = start + 86400;
+    }
+    return "http://db.sead.systems:8080/" + "466419818" + "?start_time=" + start + "&end_time=" + end + "&list_format=energy&type=P&device=" + "Panel1" + "&granularity=" + "3600";
+ 
+}
+
+function date_picker(event) {
+    var date = $("#datepicker").datepicker("getDate");
+    get_graph_data(create_url(date));
+}
+
+
+function get_graph_data(url) {
+    var test_request = new XMLHttpRequest();
+    test_request.onreadystatechange = function() {
+        //console.log(test_request.readyState);
+        if (test_request.readyState == XMLHttpRequest.DONE) {
+            if (test_request.status == 200) {
+                //console.log(test_request.responseText);
+                generate_day_chart(JSON.parse(test_request.responseText));
+            } else {
+                console.log("it broke");
+            }
         }
-    }
-};
-test_request.open("GET", url, true);
-test_request.send();
+    };
 
 
-var pie = c3.generate({
-    bindto: '#chart',
-    data: {
-        // iris data from R
-        columns: [
-            ['data1', 30],
-            ['data2', 120],
-        ],
-        type : 'pie',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-    }
-});
-
+    test_request.open("GET", url, true);
+    test_request.send();
+}
 
 var chart1 = null;
 
-function generate_chart1(data) {
+function generate_day_chart(data) {
     c3.generate({
-    bindto: '#chart2',
+    bindto: '#chart',
     data: { 
         x: 'x',
         xFormat: '%Y-%m-%d %H:%M:%S',   
@@ -16782,7 +16790,7 @@ function generate_chart1(data) {
                 }
             )),
 
-            ['data'].concat(data.data.map(
+            ['energy (kW)'].concat(data.data.map(
                 function(x) {
                     return x.energy;
                 }
@@ -16794,14 +16802,33 @@ function generate_chart1(data) {
                 type: 'timeseries',
                 tick: {
                     // this also works for non timeseries data
-                    format: '%m-%d %H:%M'
+                    format: '%H:%M'
                 }
             }
         }    
     });
 }
 
-
+$(document).ready(function() {
+    //onload
+    $("#daily_button").on("click", date_picker);
+    $("#datepicker").datepicker()
+    get_graph_data(create_url());
+});
+//var pie = c3.generate({
+//     bindto: '#chart2',
+//     data: {
+//         // iris data from R
+//         columns: [
+//             ['data1', 30],
+//             ['data2', 120],
+//         ],
+//         type : 'pie',
+//         onclick: function (d, i) { console.log("onclick", d, i); },
+//         onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+//         onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+//     }
+// });
 
 
 
